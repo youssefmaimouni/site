f6c924789d2b5b4f23b37d3818a3052695d441cc
mohamed <mehdaoui200309@gmail.com>
2024-06-19 14:28:28 +0100

    "Updated login and register views to retain input values, added favicon, refactored welcome view, and added new route for creat site"

---
+++ resources/views/creat_site.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="images/favicon.ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="{{asset('img/logo.png')}}" rel="icon">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        laravel: "#ef3b2d",
+                    },
+                },
+            },
+        };
+    </script>
+    <title>UniCollab Profile</title>
+</head>
+<style>
+    
+    .tabAnim {
+    z-index: 1;
+    }
+
+    #private:checked~div {
+        --tw-translate-x: 0%;
+    }
+
+    #public:checked~div {
+        --tw-translate-x: 100%;
+    }
+    .profile-pic {
+        border-radius: 50%;
+        height: 150px;
+        width: 150px;
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: multiply;
+        vertical-align: middle;
+        text-align: center;
+        color: transparent;
+        transition: all .3s ease;
+        text-decoration: none;
+        cursor: pointer;
+        border: solid 1px black;
+    }
+
+    .profile-pic:hover {
+        background-color: rgba(0, 0, 0, .5);
+        z-index: 10000;
+        color: #fff;
+        transition: all .3s ease;
+        text-decoration: none;
+    }
+
+    .profile-pic span {
+        display: inline-block;
+        /* color: black; */
+        padding-top: 4.5em;
+        padding-bottom: 4.5em;
+    }
+
+    form input[type="file"] {
+        display: none;
+        cursor: pointer;
+    }
+</style>
+
+<body class="mb-48 bg-gray-100">
+    <nav class="flex justify-between items-center mb-4 h-16 bg-laravel">
+        <a href="index.html"
+            ><img class="w-24" src="images/logo.png" alt="" class="logo"
+        /></a>
+        <ul class="flex space-x-6 mr-6 text-lg text-white">
+            <li>
+                <a href="/logout"
+                    ><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                    LogOut</a
+                >
+            </li>
+        </ul>
+    </nav>
+
+    @php
+        // dd($group->title);
+    @endphp
+
+    <main>
+        <div class="mx-4">
+            <div class="bg-gray-50 border border-gray-200 shadow-md p-10 rounded max-w-lg mx-auto mt-24">
+                <header class="text-center">
+                    <h2 class="text-3xl font-bold uppercase mb-1">
+                        Edit Group
+                    </h2>
+                </header>
+
+                <form action="" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-6">
+                        <center>
+                            <label for="fileToUpload">
+                                    <div class="profile-pic" id="photo" style="background-image: url('logo.jpg')">
+                                        <!-- <span class="glyphicon glyphicon-camera"></span> -->
+                                        <span>Add Image</span>
+                                    </div>
+                            </label>
+                        </center>
+                    </div>
+                    <input type="File" name="fileToUpload" accept="image/png, image/gif, image/jpeg" id="fileToUpload">
+                    <div class="mb-6">
+                        <label for="title" class="inline-block text-lg mb-2">Website Title </label>
+                        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
+                            placeholder="title"  />
+                        @error('title')
+                            <p class="text-red-500 test-xs mt-1">{{$message}}</p>
+                        @enderror                    
+                    </div>
+                    
+                    <div class="mb-6">
+                        <label for="company" class="inline-block text-lg mb-2">Link </label>
+                        <input type="url" class="border border-gray-200 rounded p-2 w-full" name="lien"
+                            placeholder="Lien"  />
+                        @error('lien')
+                            <p class="text-red-500 test-xs mt-1">{{$message}}</p>
+                        @enderror                    
+                    </div>
+                    <div class="mb-6">
+                        <label for="tite" class="inline-block text-lg mb-2">Website description </label>
+                        <textarea name="description" id="" class="border border-gray-200 rounded p-2 w-full h-52" placeholder="description"></textarea>
+                        @error('description')
+                            <p class="text-red-500 test-xs mt-1">{{$message}}</p>
+                        @enderror                    
+                    </div>
+
+                    <div class="mb-6">
+                        <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-laravel2 text-lg">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+<script>
+    const img = document.querySelector('#photo');
+    const file = document.querySelector('#fileToUpload');
+    file.addEventListener('change', function () {
+        const choosedFile = this.files[0];
+
+        if (choosedFile) {
+
+            const reader = new FileReader();
+
+            reader.addEventListener('load', function () {
+                img.setAttribute('src', reader.result);
+                img.setAttribute('style', "background-image: url('" + reader.result + "')");
+            });
+
+            reader.readAsDataURL(choosedFile);
+
+        }
+    });
+</script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,15 @@ Route::get('/', function () {
     return view('welcome');
 })->name('index')->middleware('auth');
 
+Route::get('/creat', function () {
+    return view('creat_site');
+})->middleware('auth');
+
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/authentification', [LoginController::class, 'authentification'])->name('authentification');
 
 Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::post('/store', [UserController::class, 'store'])->name('store');
 Route::post('/store', [UserController::class, 'store'])->name('store');
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
