0c1c14ca5e59665aa5e96f7d4f1cd4109a968cab
mohamed <mehdaoui200309@gmail.com>
2024-06-19 11:20:16 +0100

    "Added auth middleware to root route, and added login and register routes"

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth')->name('index');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
