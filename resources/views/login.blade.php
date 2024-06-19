35be814a9365cf75445623217d52c452161965f2
mohamed <mehdaoui200309@gmail.com>
2024-06-19 11:58:44 +0100

    hadik

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -69,6 +69,9 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
                             />
+                            @error('password')
+                            <p class="text-red-500 text-xs w-80 mt-1">{{$message}} </p>
+                            @enderror
                         </div>
 
                         <div class="mb-6">
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('auth')->name('index');
+})->name('index');
 
 Route::get('/login', function () {
     return view('login');
