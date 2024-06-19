29f216652d4affebcb454a4ec7ee840c3f6fdfcd
mohamed <mehdaoui200309@gmail.com>
2024-06-19 11:27:14 +0100

    "Updated register.blade.php: modified navigation bar, form action, and removed footer"

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -25,15 +25,21 @@
 </head>
 
 <body class="mb-48">
-    <nav class="flex justify-between items-center mb-4">
-        <a href="index.html"><img class="w-24" src="images/logo.png" alt="" class="logo" /></a>
-        <ul class="flex space-x-6 mr-6 text-lg">
+    <nav class="flex justify-between items-center mb-4 h-16 bg-laravel">
+        <a href="index.html"
+            ><img class="w-24" src="images/logo.png" alt="" class="logo"
+        /></a>
+        <ul class="flex space-x-6 mr-6 text-lg text-white">
             <li>
-                <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+                <a href="/register"
+                    ><i class="fa-solid fa-user-plus text-white"></i> Register</a
+                >
             </li>
             <li>
-                <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                    Login</a>
+                <a href="/login"
+                    ><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                    Login</a
+                >
             </li>
         </ul>
     </nav>
@@ -48,7 +54,7 @@
                     <p class="mb-4">Create an account to post gigs</p>
                 </header>
 
-                <form action="">
+                <form action="/store">
                     <div class="mb-6">
                         <label for="name" class="inline-block text-lg mb-2">
                             Name
@@ -102,13 +108,6 @@
             </div>
         </div>
     </main>
-
-    <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
-        <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
-
-        <a href="create.html" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
-    </footer>
 </body>
 
 </html>
