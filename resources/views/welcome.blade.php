f89e88a8e4c86d903b056cf2c8828db37f448c16
mohamed <mehdaoui200309@gmail.com>
2024-06-20 15:55:39 +0100

    "Refactored link and SVG elements in welcome.blade.php"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -169,23 +169,23 @@
                     </div>
                     <div class="">
                         <h3 class="text-2xl flex">
-                            <div class="w-full">
-                                <abbr title="{{$item->lien}}" class="no-underline">
-                                    {{$item->titre}}
-                                </abbr>
+                            <div class="w-full">                            
+                                {{$item->titre}}
                             </div>
                             <div class="w-12">
-                                <a href="{{$item->lien}}" target="_blank">
-                                    <?xml version="1.0" encoding="iso-8859-1"?>
-                                    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-                                    <svg fill="#000000" height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-                                        viewBox="0 0 491.1 491.1" xml:space="preserve">
-                                    <g>
-                                        <path d="M379.25,282.85l-192.8,192.8c-20.6,20.6-54,20.6-74.6,0s-20.6-54,0-74.6l155.5-155.5l-155.5-155.5
-                                            c-20.6-20.6-20.6-54,0-74.6s54-20.6,74.6,0l192.8,192.8C399.85,228.85,399.85,262.25,379.25,282.85z"/>
-                                    </g>
-                                    </svg>
-                                </a>
+                                <abbr title="{{$item->lien}}" class="no-underline">
+                                    <a href="{{$item->lien}}" target="_blank">
+                                        <?xml version="1.0" encoding="iso-8859-1"?>
+                                        <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+                                        <svg fill="#000000" height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                                            viewBox="0 0 491.1 491.1" xml:space="preserve">
+                                        <g>
+                                            <path d="M379.25,282.85l-192.8,192.8c-20.6,20.6-54,20.6-74.6,0s-20.6-54,0-74.6l155.5-155.5l-155.5-155.5
+                                                c-20.6-20.6-20.6-54,0-74.6s54-20.6,74.6,0l192.8,192.8C399.85,228.85,399.85,262.25,379.25,282.85z"/>
+                                        </g>
+                                        </svg>
+                                    </a>
+                                </abbr>
                             </div>
                         </h3>
                         
