94826a9171a2e06a8684b44d0eac71c8833286c1
mohamed <mehdaoui200309@gmail.com>
2024-06-21 15:37:37 +0100

    "Added 'Add Categorier' link and form to welcome.blade.php"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,6 +129,42 @@
                         Add Site
                     </p>
                 </a>
+                <a href="/create" class="flex text-laravel font-medium text items-center  pl-4 mt-6">
+                    <abbr title="ajouter site">
+                        <svg fill="#1967D2" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                            viewBox="0 0 27.963 27.963" xml:space="preserve">
+                        <g>
+                            <g id="c140__x2B_">
+                                <path d="M13.98,0C6.259,0,0,6.26,0,13.982s6.259,13.981,13.98,13.981c7.725,0,13.983-6.26,13.983-13.981
+                                C27.963,6.26,21.705,0,13.98,0z M21.102,16.059h-4.939v5.042h-4.299v-5.042H6.862V11.76h5.001v-4.9h4.299v4.9h4.939v4.299H21.102z
+                                "/>
+                            </g>
+                            <g id="Capa_1_9_">
+                            </g>
+                        </g>
+                        </svg>
+                    </abbr>
+                    <p class="ml-4">
+                        Add Categorier
+                    </p>
+                </a>
+                <form action="" class="flex items-center justify-around w-full">
+                    <input type="text" name="categorier" class="flex text-laravel font-medium text items-center border-laravel border focus:border-0 ml-4 my-3 w-44 ">
+                    <button type="submit" class="bg-laravel p-1 h-fit w-fit rounded-md">
+                        <svg fill="#ffff" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                            viewBox="0 0 27.963 27.963" xml:space="preserve">
+                        <g>
+                            <g id="c140__x2B_">
+                                <path d="M13.98,0C6.259,0,0,6.26,0,13.982s6.259,13.981,13.98,13.981c7.725,0,13.983-6.26,13.983-13.981
+                                C27.963,6.26,21.705,0,13.98,0z M21.102,16.059h-4.939v5.042h-4.299v-5.042H6.862V11.76h5.001v-4.9h4.299v4.9h4.939v4.299H21.102z
+                                "/>
+                            </g>
+                            <g id="Capa_1_9_">
+                            </g>
+                        </g>
+                        </svg>
+                    </button>
+                </form>
                 <div class="h-px  w-11/12 mt-4 bg-laravel">‎ </div>
                 @endauth
                 <a href="/create" class="flex text-laravel font-medium text items-center  pl-4 mt-6">
